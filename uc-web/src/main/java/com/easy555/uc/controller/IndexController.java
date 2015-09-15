ac8850fe452326fd55812023bb7b5b00bcf54067
xiangdong <xiang.d@vip.163.net>
2015-09-15 16:33:51 +0800

    Spring mvc OK.

---
+++ uc-web/src/main/java/com/easy555/uc/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.easy555.uc.controller;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+
+public class IndexController { 
+	
+	@RequestMapping("index") 
+    public String index(Model model) { 
+    	
+		model.addAttribute("message", "test");
+		
+        return "index";
+    } 
+ 
+}
