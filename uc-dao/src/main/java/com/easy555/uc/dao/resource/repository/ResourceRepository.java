22762471c03ac878aa5b9515be0f385149da5708
xiangdong <xiang.d@vip.163.net>
2015-09-21 18:04:19 +0800

    service

---
+++ uc-dao/src/main/java/com/easy555/uc/dao/resource/repository/ResourceRepository.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2005-2012 https://github.com/zhangkaitao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+package com.easy555.uc.dao.resource.repository;
+
+import com.easy555.common.repository.BaseRepository;
+import com.easy555.uc.dao.resource.entity.Resource;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 13-2-4 下午3:00
+ * <p>Version: 1.0
+ */
+public interface ResourceRepository extends BaseRepository<Resource, Long> {
+}
---
+++ uc-web/src/main/java/com/easy555/uc/controller/permission/PermissionController.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2005-2012 https://github.com/zhangkaitao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+package com.easy555.uc.controller.permission;
+
+import com.easy555.common.Constants;
+import com.easy555.common.entity.enums.AvailableEnum;
+import com.easy555.common.web.controller.BaseCRUDController;
+import com.easy555.uc.dao.permission.entity.Permission;
+import com.easy555.uc.service.permission.PermissionService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 13-1-28 下午4:29
+ * <p>Version: 1.0
+ */
+@Controller
+@RequestMapping(value = "/admin/sys/permission/permission")
+public class PermissionController extends BaseCRUDController<Permission, Long> {
+
+    public PermissionController() {
+        setResourceIdentity("sys:permission");
+    }
+
+    @Override
+    protected void setCommonData(Model model) {
+        super.setCommonData(model);
+        model.addAttribute("availableList", AvailableEnum.values());
+    }
+
+    @RequestMapping(value = "/changeStatus/{newStatus}")
+    public String changeStatus(
+            HttpServletRequest request,
+            @PathVariable("newStatus") Boolean newStatus,
+            @RequestParam("ids") Long[] ids
+    ) {
+
+        this.permissionList.assertHasUpdatePermission();
+
+        for (Long id : ids) {
+            Permission permission = baseService.findOne(id);
+            permission.setShow(newStatus);
+            baseService.update(permission);
+        }
+
+
+        return "redirect:" + request.getAttribute(Constants.BACK_URL);
+    }
+
+
+}
